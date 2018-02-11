2677bdda9bb07a2fc21036b72e160fe0c564f8e0
rinormaloku <rinormaloku37@gmail.com>
2018-02-11 14:08:00 +0100

    organized the files

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://192.168.99.100:31691/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
